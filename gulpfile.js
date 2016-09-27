898b89896fd0d3c9c0a48d20a71ccd8b1cf8711b
Przemysław Kołodziejczyk <eshlox@vertolabs.com>
2016-09-27 21:35:35 +0200

    Clean up in package.json and gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var cleanCSS = require('gulp-clean-css');
-var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
-var merge = require('merge-stream');
 var uglify = require('gulp-uglify');
 
 gulp.task('sass', function () {
@@ -14,25 +12,11 @@ gulp.task('sass', function () {
     .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(cleanCSS({compatibility: 'ie8'}));
-
-  // var cssStream = gulp.src('node_modules/offside-js/dist/offside.css')
-  //   .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
-  //   .pipe(cleanCSS({compatibility: 'ie8'}));
-
-  var mergedStream = merge(scssStream)
-    .pipe(concat('styles.css'))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('static/css'));
-
-  return mergedStream;
 });
 
 gulp.task('scripts', function() {
   return gulp.src([
-    // 'node_modules/offside-js/dist/offside.js',
     'node_modules/slideout/dist/slideout.js',
-    // 'node_modules/minigrid/src/index.js',
-    'node_modules/headroom.js/dist/headroom.js',
     '_static/scripts/scripts.js'
   ])
     .pipe(concat('scripts.js'))
@@ -53,6 +37,5 @@ gulp.task('images', function() {
   ])
     .pipe(gulp.dest('static/images'));
 });
-
 
 gulp.task('default', ['sass', 'scripts', 'fonts', 'images']);
